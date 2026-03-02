8e52bc570906564bdfdc13d48423bc12b9f7850d
chamered <202772436+chamered@users.noreply.github.com>
2026-03-02 13:54:49 +0100

    Added first char of the first two words of the name as profile icon

--- src/components/ProfileIcon.svelte
+++ src/components/ProfileIcon.svelte
@@ -13,8 +13,22 @@
 
         return COLORS[Math.floor(Math.random() * COLORS.length)];
     }
+
+    function setupProfileIcon(name) {
+        const NAMES = name.split(" ");
+        let result = "";
+        
+        let length = 0;
+        NAMES.forEach(element => {
+            if (length >= 2) return result;
+            result += element.charAt(0);
+            length++;
+        });
+
+        return result;
+    }
 </script>
 
 <div class={`${randomBgColor()} rounded-circle d-flex justify-content-center align-items-center`} style="width: 45px; height: 45px;">
-    <span class="fw-bold">{name.charAt(0)}</span>
+    <span class="fw-bold">{setupProfileIcon(name)}</span>
 </div>
